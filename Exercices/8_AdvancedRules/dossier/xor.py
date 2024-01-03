918f00ced87b2eee035ccee7d947ae7b6e9c4b00
LR-HMD <lrineau415@headmind.com>
2024-01-03 17:58:31 +0100

    Update xor.py

--- Exercices/8_AdvancedRules/dossier/xor.py
+++ Exercices/8_AdvancedRules/dossier/xor.py
@@ -1,5 +1,15 @@
+import os
+
 def xor_text(input_file, output_file, key):
     try:
+        # Vérification de l'existence du fichier d'entrée
+        if not os.path.exists(input_file):
+            raise FileNotFoundError(f"Le fichier d'entrée '{input_file}' est introuvable.")
+
+        # Vérification de l'existence du fichier de sortie
+        if not os.path.exists(output_file):
+            raise FileNotFoundError(f"Le fichier de sortie '{output_file}' est introuvable.")
+
         # Lecture du fichier d'entrée
         with open(input_file, 'r') as file:
             content = file.read()
@@ -12,8 +22,8 @@ def xor_text(input_file, output_file, key):
             file.write(encrypted_content)
 
         print(f"Le fichier chiffré '{output_file}' a été créé avec succès.")
-    except FileNotFoundError:
-        print("Le fichier spécifié est introuvable.")
+    except FileNotFoundError as e:
+        print(f"Erreur : {e}")
     except Exception as e:
         print(f"Une erreur s'est produite : {e}")
 
